f834bf33e1caba67e61b7e206ca16e0970984140
fico <breederfico@yandex.com>
2021-12-30 12:09:52 +0800

    opensea trader subgraph

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,3 +1,31 @@
-import { BigInt } from "@graphprotocol/graph-ts"
-import { ZeroExFeeWrapper } from "../generated/ZeroExFeeWrapper/ZeroExFeeWrapper"
-import { ExampleEntity } from "../generated/schema"
+// import { BigInt } from "@graphprotocol/graph-ts"
+import { log } from '@graphprotocol/graph-ts'
+import { MatchOrdersCall } from "../generated/ZeroExFeeWrapper/ZeroExFeeWrapper"
+import { Buyer, Saler } from "../generated/schema"
+
+export function handleMatchOrders(call: MatchOrdersCall): void {
+    let buyerAddr = call.inputs.leftOrder.makerAddress.toHexString();
+    let salerAddr = call.inputs.rightOrder.makerAddress.toHexString();
+    let tx = call.transaction.hash;
+
+    log.debug('====> buyer {} saler {} tx {}', [buyerAddr, salerAddr, tx.toHexString()])
+
+    // update buyer
+    let buyer = Buyer.load(buyerAddr);
+    if (buyer == null) {
+        buyer = new Buyer(buyerAddr);
+        buyer.transactions = new Array();
+    }
+    buyer.transactions.push(tx);
+    buyer.save();
+
+    // update saler
+    let saler = Saler.load(salerAddr);
+    if (saler == null) {
+        saler = new Saler(salerAddr);
+        saler.transactions = new Array();
+    }
+    saler.transactions.push(tx);
+    saler.save();
+}
+
